cd4380094fa736f1eb8d415f4feaa7ca98bfc11e
DieuPlot <dieuplot1998@gmail.com>
2022-03-02 10:03:13 +0700

    h

---
+++ DemoLoginFramework/DemoLoginFramework/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoLoginFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String user = txtUserName.Text;
+            String pass = txtPassword.Text;
+            if(user == "admin" && pass=="admin")
+            {
+                MessageBox.Show("Successful");
+            }
+            else
+            {
+                MessageBox.Show("Fail");
+
+            }
+        }
+    }
+}
